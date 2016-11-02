b97017f780269ae98dccffc4f563cb33d5ab0905
erictseng611 <etseng@ucsd.edu>
2016-11-01 22:06:15 -0700

    login

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -3,6 +3,10 @@ function initialize() {
             center: new google.maps.LatLng(32.880835, -117.237570),
             mapTypeId: google.maps.MapTypeId.ROADMAP,
             zoom: 15
-          };	 
+          };
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 };
+
+$('.message a').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
